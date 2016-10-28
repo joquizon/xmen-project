0651b561204a93e27eba810be36b1fef676717ba
jose quizon <chicoq@MariHost02.maritime.local>
2016-10-28 01:27:45 -0400

    new

--- xdb.js
+++ xdb.js
@@ -1,155 +1,37 @@
 $(document).ready(function() 
-{
-
+{$(this).scrollTop(0);
+ // $(document).ajaxSend(function(e, xhr, opt){
+ //        $("#xlogo").attr("src","placeholder.png");
+ //    });
+    
 var charnames = [ 
-"banshee",
-"professorx",
-"professorx",
-"marvelgirl",
-"marvelgirl", 
-"phoenixforce",
-"rachelsummers",
-"rachelsummers",
-"rachelsummers", 
-"angel",
-"angel",
-"psylocke", 
-"psylocke", 
-"psylocke", 
-"beast",
-"beast",
-"beast",
-"beast",
-"beast",
-"beast",
-"dazzler",
-"moiramactaggert",
-"moiramactaggert",
-"moiramactaggert",
-"moiramactaggert",
-"moiramactaggert",
-"moiramactaggert",
-"moiramactaggert",
-"moiramactaggert",
-"moiramactaggert",
-"amandasefton",
-"amandasefton",
-"amandasefton",
-"amandasefton",
-"sunder",
-"legion",
-"sharonfriedlander",
-"tomcorsi", 
-"tomcorsi", 
-"alysandestuart",
-"multipleman",
-"multipleman",
-"multipleman",
-"multipleman",
-"siryn",
-"siryn",
-"siryn",
-"jubilee",
-"gambit",
-"gambit",
-"bishop", 
-"cannonball",
-"cannonball",
-"cannonball",
-"ceciliareyes",
-"ceciliareyes",
-"cable",
-"nealshaara",
-"moonstar",
-"moonstar",
-"moonstar",
-"moonstar",
-"northstar",
-"frenzy",
-"changeling","longshot","colossus","cyclops","forge","havok","iceman","lockheed","magneto","mimic","multiple man","nightcrawler","phoenix","polaris","rachel summers","revanche","rogue","shadowcat","storm","sunfire","thunderbird","wolverine","husk","slipstream","stacy x","chamber","xorn","white queen","sunpyre","wraith","omerta","sage","skrull","maggott","marrow","cecilia reyes","joseph"];
+"marrow","maggott","banshee","professorx","professorx","marvelgirl","marvelgirl", "phoenixforce","rachelsummers","rachelsummers","rachelsummers", "angel","angel","psylocke", "psylocke", "psylocke", "beast","beast","beast","beast","beast","beast","dazzler","moiramactaggert","moiramactaggert","moiramactaggert","moiramactaggert","moiramactaggert","moiramactaggert","moiramactaggert","moiramactaggert","moiramactaggert","amandasefton","amandasefton","amandasefton","amandasefton","sunder","legion","sharonfriedlander","tomcorsi", "tomcorsi","alysandestuart","multipleman","multipleman","multipleman","multipleman","siryn","siryn","siryn","jubilee","gambit","gambit","bishop", "cannonball","cannonball","cannonball","ceciliareyes","ceciliareyes","cable","nealshaara","moonstar","moonstar","moonstar","moonstar","northstar","northstar","frenzy","omerta","omerta","whitequeen","whitequeen","whitequeen","chamber","chamber","chamber","sunpyre","sunpyre","stacyx","stacyx","stacyx","lifeguard","slipstream","husk","juggernaut","fantomex","fantomex","eva","eva","eva","beak","beak","dust","stepfordcuckoos","stepfordcuckoos","stepfordcuckoos","stepfordcuckoos","stepfordcuckoos","stepfordcuckoos","shenxorn","mystique","vulcan","darwin","cyclops","sway","warpath","warpath",
+"warpath","ladymastermind","ladymastermind","omegasentinel","omegasentinel","sabretooth","armor","armor","pixie","karma","sunspot","sunspot","aurora","magma","magma","madisonjeffries","drnemesis","drnemesis","drnemesis","magik","namor","namor","danger","domino","cloak","dagger","x23","x23","x23","hopesummers","warbird","firestar","firestar","iceman","iceman","m","forgetmenot","colossus","colossus","magneto","magneto","nightcrawler","rogue","polaris","shadowcat","shadowcat","changeling","havok","wolverine","wolverine","storm","sunfire","thunderbird",
+"krakoa","ariel","doop","hepzibah","longshot","petra","forge","lockheed","mimic","multiple man","phoenix","rachel summers","revanche","xorn","wraith","sage","skrull","joseph",
+
+];
 
 var charnames2 = [
-"sean cassidy",
-"professor x",
-"charles xavier",
-"jean grey",
-"marvel girl",
-"phoenix force",
-"rachel summers",
-"rachel grey",
-"phoenix",
-"archangel",
-"warren kenneth worthington III", 
-"betsy braddock", 
-"elizabeth braddock", 
-"elizabeth betsy braddock",
-"henry mccoy",
-"hank mccoy",
-"dr. hank mccoy",
-"dr. henry mccoy",
-"henry hank mccoy",
-"hank philip mccoy",
-"alison blaire",
-"moira mactaggert",
-"moira mactaggart", 
-"moira mctaggert",
-"moira mctaggart",
-"dr. moira mactaggert",
-"dr. moira mactaggart", 
-"dr. moira mctaggert",
-"dr. moira mctaggart",
-"moira kinross mctaggert",
-"amanda sefton",
-"jimaine szardos",
-"daytripper",
-"second magik",
-"mark hallett",
-"david charles haller",
-"sharon friedlander",
-"thomas corsi",
-"tom corsi",
-"alysande stuart",
-"multiple man",
-"james jamie madrox",
-"jamie madrox",
-"james madrox", 
-"theresa maeve rourke cassidy",
-"theresa cassidy",
-"theresa rourke cassidy",
-"jubilation lee",
-"remy lebeau",
-"remy etienne lebeau",
-"lucas bishop",
-"sam guthrie",
-"samuel guthrie",
-"samuel zachary guthrie",
-"cecilia reyes",
-"dr. cecilia reyes",
-"nathan summers",
-"neal shaara",
-"danielle moonstar",
-"psyche",
-"mirage",
-"dani moonstar",
-"jean paul beaubier",
-"joanna cargill",]
+"sarah rushman","japheth","sean cassidy","professor x","charles xavier","jean grey","marvel girl","phoenix force","rachel summers","rachel grey","phoenix","archangel","warren kenneth worthington iii", "betsy braddock", "elizabeth braddock", "elizabeth betsy braddock","henry mccoy","hank mccoy","dr. hank mccoy","dr. henry mccoy","henry hank mccoy","hank philip mccoy","alison blaire","moira mactaggert","moira mactaggart", "moira mctaggert","moira mctaggart","dr. moira mactaggert","dr. moira mactaggart", "dr. moira mctaggert","dr. moira mctaggart","moira kinross mctaggert","amanda sefton","jimaine szardos","daytripper","second magik","mark hallett","david charles haller","sharon friedlander","thomas corsi","tom corsi","alysande stuart","multiple man","james jamie madrox","jamie madrox","james madrox", "theresa maeve rourke cassidy","theresa cassidy","theresa rourke cassidy","jubilation lee","remy lebeau","remy etienne lebeau","lucas bishop","sam guthrie","samuel guthrie","samuel zachary guthrie","cecilia reyes","dr. cecilia reyes","nathan summers","neal shaara","danielle moonstar","psyche","mirage","dani moonstar","jean paul beaubier","jean paul beaus","joanna cargill","paulie provenzano","paul provenzano","white queen","emma frost","emma grace frost","jono starsmore","jonothon starsmore","jonothon evan starsmore","leyu yoshida","yoshida reyu","stacy x","ripcord","miranda leevald","heather cameron",
+"davis cameron","paige guthrie","cain marko","charlie cluster 7","jean phillipe","e.v.a","eva bell","eva","blackwing","barnell bohusk","sooraya qadir","stepford cuckoos","celest cuckoo","esme cuckoo","irma cuckoo","phoebe cuckoo","sophie cuckoo","shen xorn","raven darkholme","gabriel summers","armando munoz","scott summers","suzanne chan","james proudstar","thunderbird 2","thunderbird ii","regan wyngarde","lady mastermind","karima shapandar","omega sentinel","victor creed","hichako isaki","isaki hichako","megan gwyn","xian coy manh","bobby da costa","robert da costa",
+"jeanne marie beaubier","amara aquilla","alison crestmere","madison jeffries","dr. nemesis","doctor nemesis","james bradley","illyana rasputin","namor","namor the submariner","danger room","neena thurman","tyrone johnson","tandy bowen","laura kinney","x 23","x-23","hope summers","ava'dara naganandini","angelica jones","angel jones","bobby drake","robert drake","monet st. croix","forget me not","piotr nikolaievitch rasputin","piotr rasputin","erik lensherr","max eisenhardt","kurt wagner","anna marie","lorna dane","kitty pryde","katherine pryde","kevin sydney","alex summers","logan howlett","james howlett","ororo monroe","shiro yoshida","john proudstar",
+
+]
+
+
 
 function namer()
-	{
-		var x = document.getElementById("charnames").value;
-
-		$("#datacontain").fadeOut(200);
+	{	
+		var x = $("#charnames").val().toLowerCase();
+		// var x = document.getElementById("charnames").value;
+		$("#mainholder").css("margin-left","2%");
+		$("#datacontain").fadeOut(2000);
 	 	$("#info").animate({height: "0px",width: "0px"},200);
+	 	$("#info2").css("top","0px");
 	 	$("#firstapp").animate({height: "0",width:"0px"},200);
-		$("#pic").fadeOut(100);
-		$("#pic").animate({height: "50px",width: "50px"},225);
-		$("#marquee").animate({ marginTop: "206px",marginLeft: "21px"},200);
-		$("#xlogo").fadeOut(300);
-		$("#pic").attr("src","placeholder.png")
-		$("#pic").fadeIn(100);
+		$("#xlogo").attr("src","placeholder.png");
 		$("#pic").animate({height: "300px",width: "300px"},200);
-		$("#marquee").animate({ marginTop: "303px",marginLeft: "60px"},200)
-
+		$("#marquee").animate({ marginTop: "303px",marginLeft: "60px"},200);
 
 //  below only happens if the name is correct. therefore the place holder image hidden by the x logo is always saying ERROR. BUT if it's 
 // not an error then below happens. this solves the error message for the user who uses wrong name. w/o having to run error message
@@ -158,58 +40,187 @@ function namer()
 // multiple word names are to be placed in charnames2 array and renamed in charnames in one word. files to be called img's and txt are one word
 // as well.
 
+
      for (var t = 0; t <= charnames.length; t++)
      for (var v = 0; v <= charnames2.length; v++)	
 
 	 if (charnames2[v] === x)
 	 	{ 
-	 		$("#pic").attr("src","profilepic/"+charnames[v]+".png");
+	 		$("#error").css("display","block");
+	 		$("#xlogo").attr("src","profilepic/"+charnames[v]+".png");
 	 		$("#cover").attr("src","covers/"+charnames[v]+".jpg");
-	 		$("#info").load("info/"+charnames[v] +".txt");
-	 		$("#datacontain").fadeIn(200);
-	 		$("#info").animate({height: "500px",width: "400px"},200);
-	 		$("#firstapp").animate({height: "500px",width:"400px"},200);
+	 		$("#info2").css("right","0px","top", "0px");
+	 		
+	 		$("#datacontain").fadeIn(2000);
+	 		$("#error").css("display","none");
+	 		$("#info").animate({height: "504px",width: "400px"},200);
+	 		$("#firstapp").animate({height: "504px",width:"400px"},200);
+	 		$("#info2").load("info/"+charnames[v] +".txt");
+	 		$("#info2").draggable({ axis: "y" });
+	 		$("#info2").css("left","0px","top", "0px");	
+	 		
+	 		
 	 	}
 	 else if (charnames[t] === x)
 	 	{
-	 		$("#pic").attr("src", "profilepic/"+charnames[t]+".png");
+	 		$("#error").css("display","block");
+	 		$("#xlogo").attr("src", "profilepic/"+charnames[t]+".png");
 	 		$("#cover").attr("src","covers/"+charnames[t]+".jpg");
-	 		$("#info").load( "info/"+charnames[t] +".txt");
-	 		$("#datacontain").fadeIn(200);
-	 		$("#info").animate({height: "500px",width: "400px"},200);
-	 		$("#firstapp").animate({height: "500px",width:"400px"},200);
-	 	};
-
-	 $("#charnames").val("name");
-
-
-
-
+	 		$("#info2").css("right","0px","top", "0px");
+	 		
+	 		$("#datacontain").fadeIn(2000);
+	 		$("#error").css("display","none");
+	 		$("#info").animate({height: "504px",width: "400px"},200);
+	 		$("#firstapp").animate({height: "504px",width:"400px"},200);
+	 		$("#info2").load( "info/"+charnames[t] +".txt");
+	 		$("#info2").draggable({ axis: "y" });	 		
+	 		$("#info2").css("left","0px","top", "0px");
+	 	}
+	 	else
+	 	{
+	 		console.log("dude")
+	 	}	
+	 	
+	 $("#charnames").val("");
 
 		};
 
+	 
+	 		 		
+
+    $(".tlinepic").click(function(event) 
+    {
+       var z = (event.target.id);
+       var y = (event.target.className)
+       var par =(event.target.parentNode.parentNode.id);
+       var parwid = $("#"+z).parent().parent().width();
+      
+
+       if (parwid === 202)
+       {
+       $("#"+z).parent().parent().animate({width: "535px"},1000);
+       $("#"+z).parent().parent().parent().animate({width: "587px"},1000);
+       }
+       else if (parwid === 100)
+       {
+       $("#"+z).parent().parent().animate({width: "433px"},1000);
+       $("#"+z).parent().parent().parent().animate({width: "463px"},1000);
+       }
+       else if (parwid === 305)
+       {
+       	$("#"+z).parent().parent().animate({width: "637px"},1000);
+       };
+	
+		$("#"+y+par+"lister").load( "brief/"+z+".txt");
+		$("#"+"briefpic"+par).attr("src","tlineprof/"+z+".jpg");
+       $("#"+y+par).css("display","inline-block","float","left"); 
+
+       $("#"+y+par).attr("src","covers/"+z+".jpg").css("height","202px","width","165px");
+      $("#listholder"+par).fadeIn(2000);
+      $("#listholder"+par).css("display","inline-block","float","left");
+    });
+
+    	$(".tlinecloser").click(function(event)
+    	{ 
+			var z1 = (event.target.id);
+			var z2 = (event.target.parentNode);
+       		var par =(event.target.parentNode.parentNode.id);
+       		var parwid = $("#"+z1).parent().parent().width();
+
+       	$("#listholder"+par).css("display","none");	
+
+       		if (parwid === 535)
+       {
+       $("#"+z1).parent().parent().animate({width: "202px"},1000);
+       $("#"+z1).parent().parent().parent().animate({width: "254px"},1000);
+       }
+       else if (parwid === 433)
+       {
+       $("#"+z1).parent().parent().animate({width: "100px"},1000);
+       $("#"+z1).parent().parent().parent().animate({width: "130px"},1000);
+       }
+       else if (parwid === 637)
+       {
+       	$("#"+z1).parent().parent().animate({width: "305px"},1000);
+       };
+
+		});
 
 
 
+
+
+
+
+// create div box to animate once .tlinepic is clicked. put divs that will contain tpicinfo per xmen in html
+// multiple column years will need .parent().parent() separate class and code` 
 // Create reset button
 
 function modalreset()
 	{
+		$("#xlogo").attr("src","xlogo.png");
 		$("#datacontain").fadeOut(200);
-	 	$("#info").animate({height: "0px",width: "0px"},200);
 	 	$("#firstapp").animate({height: "0",width:"0px"},200);
-
-	 	$("#pic").fadeOut(100);
-		$("#pic").animate({height: "50px",width: "50px"},100);
-	 	
-	 	
+	 	$("#mainholder").css("margin-left","40%");
 	 	$("#xlogo").animate({height: "50px",width: "50px"},10).fadeIn(100).animate({height: "200px",width: "200px"},200);
 	 	$("#marquee").animate({ marginTop: "206px",marginLeft: "21px"},100);
-	}
+	};
+
+
+$("#tlinebutt").click(function()
+{	
+	$("#tlinebutt").css("display","none");
+	$("#dbasebutthold").css("display","block");	
+	$("#dbholder").fadeOut(1000);
+	$("#bod").css("overflow","visible");
+	$("#transit").animate({height: "0px"},1500);
+});
+
+$("#dbasebutt").click(function()
+{	
+
+	// $(".listholder").css("display","none");	
+    // $("#th1,#th6,#th26,#th28,#th30,#th35").attr("class","tlineholder2").animate({width: "202px"},1000);
+    // $("#th13a,#th23,#th31").attr("class","tlineholder3").animate({width: "305px"},1000);   
+    // $(".tlineholder").animate({width: "100px"},1000);   
+    // timeline reset button?
+	$("#transit").animate({height: "2000px"},1500);
+	$("#dbasebutthold").css("display","none");
+	$("#tlinebutt").css("display","block");	
+	$("#dbholder").delay(1000).fadeIn(1000);
+	$('html,body').scrollTop(0);
+	$("#bod").css("overflow","hidden");
+
+});
+
+
+function validator()
+{ 
+	for (var o = 0; o <= charnames.length; o++)
+     for (var q = 0;q <= charnames2.length; q++)
+var d = $("#charnames").val().toLowerCase();
+if(charnames2[q] === d)
+{
+	alert("t");
+}
+else if(charnames[o] === d)
+{
+	alert("fdf");
+};
+	 $("#charnames").val("");
+};
+
+
+$("#top").click(function()
+{
+	$("#info2").css("top","0px")
+});
 
 $("#pushbutt").click(namer);
+// $("#pushbutt").click(validator);
 $("#closer").click(modalreset);
-$("#pic").click(modalreset);
+$("#xlogo").click(modalreset);
+$("#main").draggable();
 
 $("#charnames").keypress(function(e) 
 				{
@@ -220,18 +231,6 @@ $("#charnames").keypress(function(e)
 				});
 
 
-// var x = "jose is my name";
-// console.log(x);
-// var d = 6
-// function fe(d){return(d+2-5)}
-// console.log(fe(6));
-
-// var i=0
-// for (i=0;i<=101;i+=1)
-// 	if (i%5===0 && i%3===0){console.log("FizzBuzz")}
-// 	else if (i%5===0) {console.log("buzz")}
-// 	else if (i%3===0){console.log("fizz")}
-// 	else{console.log(i)}
 
 
 });
